06ce67a3166ea101b91c0af16f3ff988932761f9
coderuzb <53489876+coderuzb@users.noreply.github.com>
2019-07-30 23:17:05 +0500

    Update index.php

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ $admin = "638611275";
     //-------------------------------------
   $inline_keyboard = [
                             [
-                                [ 'text' => "Dostlarga ulashish" , 'switch_inline_query' => "https://t.me/UzApks/"];
+                                [ 'text' => "Dostlarga ulashish" , 'switch_inline_query' => "https://t.me/UzApks/"]
                             ] ,
                        ];
     
